db2d757930571f21c040844eb001a6da142ffd41
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-09-16 18:49:47 -0400

    added about

--- routes/google.js
+++ routes/google.js
@@ -5,14 +5,18 @@ router.get('/:zoom/:x/:y', async (req, res) => {
     let zoom = req.params.zoom;
     let x = req.params.x;
     let y = req.params.y;
-    res.send((
-        await fetch(`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/${zoom}/${x}/${y}?key=AIzaSyDxn11dlm134OPDCeb18AgK5B-rjlQ7msg`)
+
+    const url = await fetch(`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/${zoom}/${x}/${y}?key=AIzaSyDxn11dlm134OPDCeb18AgK5B-rjlQ7msg`)
         .then(res =>  res.blob())
         .then(blob => {
-            console.log(blob.size);
-            return blob;
+          const newBlob = new Blob([blob], { type: "image/png" });
+          imgurl = URL.createObjectURL(newBlob);
+          console.log(imgurl);
         })
-        ))
+
+    res.json({
+      blob_url: url
+    })
 });
 
 module.exports = router;
