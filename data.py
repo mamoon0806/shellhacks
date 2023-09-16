c757f9f65e63432350849b16b2d7e62acfe69219
mamoon0806 <mamoon0806@gmail.com>
2023-09-16 10:49:36 -0400

    changed data

--- data.py
+++ data.py
@@ -1,31 +1,52 @@
 import requests
 import json
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import base64
 
 # Define the URL you want to send the POST request to
-url = "https://example.com/api/endpoint"
+API_KEY = 'AIzaSyDxn11dlm134OPDCeb18AgK5B-rjlQ7msg'
+
+mapType = 'US_AQI'
+zoom = 0
+x = 0
+y = 0
+
+url = f"https://airquality.googleapis.com/v1/mapTypes/{mapType}/heatmapTiles/{zoom}/{x}/{y}?key={API_KEY}"
+
+print(url)
 
 # Define the data you want to send in the POST request
 data = {
-    "key1": "value1",
-    "key2": "value2"
+  "universal_aqi": True,
+  "location": {
+    "latitude": 37.419734,
+    "longitude": -122.0827784
+  },
+  "extra_computations": [
+    "HEALTH_RECOMMENDATIONS",
+    "DOMINANT_POLLUTANT_CONCENTRATION",
+    "POLLUTANT_CONCENTRATION",
+    "LOCAL_AQI",
+    "POLLUTANT_ADDITIONAL_INFO"
+  ],
+  "language_code": "en"
 }
 
 # Convert the data to JSON format
 json_data = json.dumps(data)
 
-# Define the headers (if needed)
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer YourAccessToken"  # Add any required headers
-}
-
 # Send the POST request
-response = requests.post(url, data=json_data, headers=headers)
+response = requests.get(url)
+array = list(response.content)
+print(array)
+
+
 
 # Check the status code of the response
 if response.status_code == 200:  # Assuming a successful response has a status code of 200
     # Parse the JSON response
     json_response = response.json()
-    print("JSON Response:", json_response)
 else:
     print("POST request failed with status code:", response.status_code)
